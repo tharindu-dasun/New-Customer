3ca8ac7d52b7ee779bc882f3742cb460482b142c
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-09-18 22:55:12 +0530

    Initial commit

---
+++ src/test/java/lk/ijse/aad/New/Customer/NewCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.aad.New.Customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
